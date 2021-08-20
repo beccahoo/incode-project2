29e28f955f18cacb7071eecc7fd7eae6a75f0ba8
beccahoo <beccahoo@gmail.com>
2021-08-20 14:41:26 +1000

    create final draft

--- formHandler.js
+++ formHandler.js
@@ -2,10 +2,15 @@
 const name = document.querySelector("#name")
 const email = document.querySelector("#email")
 const form = document.querySelector("#contact")
+const telephone = document.querySelector("#telephone")
+const message = document.querySelector("#message")
 
 //REGEX
 const validEmail = /^[A-Za-z0-9\._\-]+@[A-Za-z]+[A-Za-z0-9\-]+[A-Za-z]+(\.[A-Za-z0-9\-]+)+$/
 const validName = /^[A-Za-zÀ-ÖØ-öø-ÿ0-9 \.'\-]+$/
+const validTelephone = /^((\+61\s?)?(\((0|02|03|04|07|08)\))?)?\s?\d{1,4}\s?\d{1,4}\s?\d{0,4}$/
+/*const validMessage = */
+
 
 form.setAttribute("novalidate", true)
 form.onsubmit = submitForm
@@ -13,21 +18,18 @@ form.onsubmit = submitForm
 function submitForm(event) {
     event.preventDefault()
     
-
-
     //if all inputs pass regex test, then print to console or load modal/popup
     //let test = validName.test (name.Value)
     //console.log(test)
     //inputValid(validName, name)
     //inputValid(validEmail, email)
 
-    if (inputValid(validName, name) && inputValid(validEmail, email)) {
-        console.log(`Name: ${name.value}\nEmail: ${email.value}`)
+    if (inputValid(validName, name) && inputValid(validEmail, email) && inputValid(validTelephone, telephone)) {
+        console.log(`Name: ${name.value}\nEmail: ${email.value}\nTelephone: ${telephone.value}`)
         modal_container.classList.add(`show`);        
     }   else{
         console.log ('Inputs are incorrect. Please try again')
     }
-    
 }
 
 function inputValid(regex, input) {
